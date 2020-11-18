1d0f7200651580ae2ca197e5775296b92b8c0f17
Nikolaos Chatzitheodorou <chatzitheodorou@intralot.com>
2020-11-18 13:42:03 +0200

    Async Writes in Redux: Add Save Course Thunk and Action Creators, Handle Creates and Updates in Reducer, Dispatch Create and Update

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { loadCourses } from "../../redux/actions/courseActions";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
 import { newCourse } from "../../../tools/mockData";
 
-function ManageCoursePage({ courses, authors, loadAuthors, loadCourses, ...props }) { // rest operator: assign rest of the properties to props object
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses, saveCourse, ...props }) { // rest operator: assign rest of the properties to props object
   const [course, setCourse] = useState({ ...props.course }); // initial state = course passed in on props
   const [errors, setErrors] = useState({});
 
@@ -32,13 +32,18 @@ function ManageCoursePage({ courses, authors, loadAuthors, loadCourses, ...props
     }));
   }
 
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course); // the course that is available in local state
+  }
+
   return (
     <CourseForm
       course={course}
       errors={errors}
       authors={authors}
       onChange={handleChange}
-      // onSave={handleSave}
+      onSave={handleSave}
     />
   );
 }
@@ -49,6 +54,7 @@ ManageCoursePage.propTypes = {
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state) { // This func determines what part of the state is passed to our component via props
@@ -61,7 +67,8 @@ function mapStateToProps(state) { // This func determines what part of the state
 
 const mapDispatchToProps = { // Object form of mapDispatchToProps
   loadCourses, // These names are the same as the unbound thunks we imported at the top. The bound action passed in on props wins.
-  loadAuthors
+  loadAuthors,
+  saveCourse
 };
 
 export default connect( // Connect conteiner component to Redux
