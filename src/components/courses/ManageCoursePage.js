e0fb2399fedc873dba4ebf8f91053d0415500db9
Nikolaos Chatzitheodorou <chatzitheodorou@intralot.com>
2020-11-18 11:29:48 +0200

    Async Writes in Redux: Create ManageCoursePage, Object Form of mapDispatchToProps, Routing for ManageCoursePage

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { connect } from "react-redux";
+import { loadCourses } from "../../redux/actions/courseActions";
+import { loadAuthors } from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+
+class ManageCoursePage extends React.Component {
+  componentDidMount() {
+    const { courses, authors, loadAuthors, loadCourses } = this.props;
+
+    if (courses.length === 0) {
+      loadCourses().catch(error => {
+        alert("Loading courses failed" + error);
+      });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch(error => {
+        alert("Loading authors failed" + error);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Manage Course</h2>
+      </>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) { // This func determines what part of the state is passed to our component via props
+  return {
+    courses: state.courses,
+    authors: state.authors
+  };
+}
+
+const mapDispatchToProps = { // Object form of mapDispatchToProps
+  loadCourses, // These names are the same as the unbound thunks we imported at the top. The bound action passed in on props wins.
+  loadAuthors
+};
+
+export default connect( // Connect conteiner component to Redux
+  mapStateToProps,
+  mapDispatchToProps // when we omit mapDispatchToProps, our component gets a dispatch prop injected automatically
+)(ManageCoursePage);
